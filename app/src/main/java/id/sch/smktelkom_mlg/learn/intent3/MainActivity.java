ed4eee18cf880264fdadd02c1777bb01d8a0b8f5
diniristanti <dini_ristanti_24rpl@student.smktelkom.com>
2017-02-02 12:16:08 +0700

    Intent Browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String phoneNumber;
     private boolean message;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +31,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.imageViewBrowser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("http://www.smktelkom-mlg.sch.id/");
+            }
+        });
+
+    }
+
+    private void openWebPage(String s) {
+
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     private void composeSmsMessage(String s) {
